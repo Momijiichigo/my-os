a57f45a74ab2e4d0bdc8d01c0df055d62bb11daa
Momijiichigo <ichigomomiji436@gmail.com>
2023-10-27 18:21:59 -0500

    breakpoint error handler

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,13 +1,17 @@
+#![feature(abi_x86_interrupt)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 use bootloader_api::{BootInfo, entry_point};
+use interrupts::init_idt;
 
 
 // mod vga_buffer;
 mod graphics;
 mod logger;
+
+pub mod interrupts;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -15,6 +19,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn init() {
+    init_idt();
+}
 
 entry_point!(kernel_main);
 
@@ -24,7 +31,10 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     let info = framebuffer.info().clone();
     logger::init_logger(framebuffer.buffer_mut(), info);
     log::info!("Hello World!");
-    
+    init();
+
+    x86_64::instructions::interrupts::int3();
+    log::info!("it did not crash");
     // for byte in framebuffer.buffer_mut() {
     //     *byte = 0x88;
     // }
